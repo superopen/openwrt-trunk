04e50c0439e1ad798ee1ec4c661d594c93ae14b4
superopen <lyj@lengyountekimbp.lan>
2014-10-09 21:19:22 +0800

    support get hiwifi mac

--- trunk/package/base-files/files/lib/functions/system.sh
+++ trunk/package/base-files/files/lib/functions/system.sh
@@ -27,6 +27,25 @@ mtd_get_mac_ascii()
 	[ -n "$mac_dirty" ] && macaddr_canonicalize "$mac_dirty"
 }
 
+mtd_get_mac_hiwifi()
+{
+        local mtdname="$1"
+        local key="$2"
+        local part
+        local mac_dirty
+
+        part=$(find_mtd_part "$mtdname")
+        if [ -z "$part" ]; then
+                echo "mtd_get_mac_ascii: partition $mtdname not found!" >&2
+                return
+        fi
+
+        mac_dirty=$(strings "$part" | sed -n 's/^'"$key"' = //p')
+
+        # "canonicalize" mac
+        [ -n "$mac_dirty" ] && macaddr_canonicalize "$mac_dirty"
+}
+
 mtd_get_mac_binary() {
 	local mtdname="$1"
 	local offset="$2"
